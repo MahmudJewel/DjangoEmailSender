45590203e6bd6845e01cd274ac1f57a64e0418ff
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-03-12 17:44:06 +0600

    Draft saved for mail

--- second/views.py
+++ second/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+
+# from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-	return render(request, 'index.html')
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			subject = "Website Inquiry" 
+			body = {
+			'first_name': form.cleaned_data['first_name'], 
+			'last_name': form.cleaned_data['last_name'], 
+			'email': form.cleaned_data['email_address'], 
+			'message':form.cleaned_data['message'], 
+			}
+			message = "\n".join(body.values())
+			print('Messages ===============> ', message)
+			try:
+				send_mail(subject, message, 'admin@example.com', ['admin@example.com']) 
+			except BadHeaderError:
+				return HttpResponse('Invalid header found.')
+			return redirect ("home")
+	form = ContactForm()
+	return render(request, 'index.html', {'form':form})
