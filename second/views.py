41f0b4243adbfa2319df70b855bc5b46960a0aaa
Mahmud Jewel <jmbengal.mbstu@gmail.com>
2023-03-13 15:23:55 +0600

    sending messages done

--- second/views.py
+++ second/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
-
+from django.conf import settings
 # from django.shortcuts import render
 # Create your views here.
 
@@ -10,17 +10,18 @@ def home(request):
 	if request.method == 'POST':
 		form = ContactForm(request.POST)
 		if form.is_valid():
-			subject = "Website Inquiry" 
+			subject = form.cleaned_data['subject'], 
+			toMail = settings.EMAIL_HOST_USER
 			body = {
-			'first_name': form.cleaned_data['first_name'], 
-			'last_name': form.cleaned_data['last_name'], 
-			'email': form.cleaned_data['email_address'], 
-			'message':form.cleaned_data['message'], 
+				'name': form.cleaned_data['name'],
+				'from_mail': form.cleaned_data['email_address'],   
+				'message':form.cleaned_data['message'], 
 			}
 			message = "\n".join(body.values())
-			print('Messages ===============> ', message)
+			print('subject: ==> ', subject[0])
+			print('Body: ==> ', type(message))
 			try:
-				send_mail(subject, message, 'admin@example.com', ['admin@example.com']) 
+				send_mail(subject[0], message, body['from_mail'], [toMail]) 
 			except BadHeaderError:
 				return HttpResponse('Invalid header found.')
 			return redirect ("home")
